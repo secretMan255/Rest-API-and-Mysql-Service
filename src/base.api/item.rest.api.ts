2bf996d98408ae26a3d256381f18bf3b99a94d2b
yiliang <yapyiliangww321@gmail.com>
2025-01-26 16:59:01 +0800

    generate jwt token and save into cookie. handle cart manage functions

--- src/base.api/item.rest.api.ts
+++ src/base.api/item.rest.api.ts
@@ -13,9 +13,15 @@ import {
      CreateAccountRequest,
      StateRequest,
      EditAccountRequest,
+     GetCartRequest,
+     CartItemRequest,
 } from '../JoiValidator/JoiValidator'
-import { getFile } from '../commond/commond'
+import { getFile, CartRequestType } from '../commond/commond'
 import { ResultType } from './index'
+import { ApiBase } from './index'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export async function OnTest(req: Request, res: Response): Promise<any> {
      return 'GOOD'
@@ -38,7 +44,25 @@ export async function OnSubscribe(req: Request, res: Response): Promise<any> {
 }
 
 export async function OnUserLogin(req: Request, res: Response): Promise<any> {
-     return await JoinValidator(req.body, async (data) => MySqlService.userLogin(data), UserLoginRequest)
+     const userData = await JoinValidator(req.body, async (data) => MySqlService.userLogin(data), UserLoginRequest)
+
+     if (!userData || userData?.err) {
+          return userData
+     }
+
+     const token = ApiBase.generateToken({ id: userData.id, user: 'customer', email: userData.email, name: userData.name })
+
+     res.cookie('authToken', token, {
+          httpOnly: true,
+          secure: false, // process.env.NODE_ENV === 'production',
+          sameSite: 'lax',
+          maxAge: 24 * 60 * 60 * 1000,
+     })
+     userData.token = token
+
+     return userData
+
+     // return await JoinValidator(req.body, async (data) => MySqlService.userLogin(data), UserLoginRequest)
 }
 
 export async function OnResetPassowrdOTP(req: Request, res: Response): Promise<any> {
@@ -62,5 +86,30 @@ export async function OnGetState(req: Request, res: Response): Promise<any> {
 }
 
 export async function OnEditAccount(req: Request, res: Response): Promise<any> {
-     return await JoinValidator(req.body, async (data) => MySqlService.editAccount(data), EditAccountRequest)
+     const decodedJwt = jwt.verify(req.cookies?.authToken, process.env.SECRET_KEY) as JwtPayload
+     return await JoinValidator(req.body, async (data) => MySqlService.editAccount(data, decodedJwt), EditAccountRequest)
+}
+
+export async function OnGetCart(req: Request, res: Response): Promise<any> {
+     const decodedJwt = jwt.verify(req.cookies?.authToken, process.env.SECRET_KEY) as JwtPayload
+     const cartPayload: CartRequestType = {
+          userId: String(req.query.id),
+          tokenId: String(decodedJwt.id),
+     }
+     return await JoinValidator(cartPayload, async (data) => MySqlService.getCart(data), GetCartRequest)
+}
+
+export async function OnAddItemCart(req: Request, res: Response): Promise<any> {
+     const decodedJwt = jwt.verify(req.cookies?.authToken, process.env.SECRET_KEY) as JwtPayload
+     return await JoinValidator(req.body, async (data) => MySqlService.addCartItem(data, decodedJwt), CartItemRequest)
+}
+
+export async function OnMinusItemCart(req: Request, res: Response): Promise<any> {
+     const decodedJwt = jwt.verify(req.cookies?.authToken, process.env.SECRET_KEY) as JwtPayload
+     return await JoinValidator(req.body, async (data) => MySqlService.minusCartItem(data, decodedJwt), CartItemRequest)
+}
+
+export async function OnRemoveItemCart(req: Request, res: Response): Promise<any> {
+     const decodedJwt = jwt.verify(req.cookies?.authToken, process.env.SECRET_KEY) as JwtPayload
+     return await JoinValidator(req.body, async (data) => MySqlService.removeCartItem(data, decodedJwt), CartItemRequest)
 }
